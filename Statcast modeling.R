a0f22b19b4319dd21b00c3cf2bc81f615251d3a9
djcunningham0 <djcunningham0@gmail.com>
2018-06-21 13:15:38 -0500

    remove misc notes

--- Statcast modeling.R
+++ Statcast modeling.R
@@ -9,7 +9,7 @@ print("Preparing data frames...")
 # batted_balls_2015-2017.RData comes from pull_pitch_data_from_statcast function
 if (!exists("original_batted") || !is.data.frame(get("original_batted"))) {
   tryCatch(#original_batted <- read.csv("./data/batted_balls_2015-2017.csv"),
-           original_batted <- load("./data/batted_balls_2015-2017.RData"),
+           load("./data/batted_balls_2015-2017.RData"),
            error=function(err) {
              print("Missing Statcast data file.")
            })
@@ -73,9 +73,9 @@ library(randomForest)
 load("./data/rf.RData")
 load("./data/rf.speed.RData")
 load("./data/rf.shift.RData")
-set.seed(1)
-which.train <- sample(1:dim(batted)[1],1e5)
-train <- batted[which.train,]
+# set.seed(1)
+# which.train <- sample(1:dim(batted)[1],1e5)
+# train <- batted[which.train,]
 # rf <- randomForest(class ~ launch_speed + launch_angle + spray_angle, data=train)
 # rf.speed <- randomForest(class ~ launch_speed + launch_angle + spray_angle + Spd, data=train)
 # rf.shift <- randomForest(class ~ launch_speed + launch_angle + spray_angle + Spd + shift, data=train)
@@ -89,13 +89,10 @@ batted <- add_preds_from_probs("rf.speed", batted, probs.rf.speed, lw)
 probs.rf.shift <- predict(rf.shift, newdata=batted, type="prob")
 batted <- add_preds_from_probs("rf.shift", batted, probs.rf.shift, lw)
 
-probs.rf.shift.2 <- predict(rf.shift.2, newdata=batted, type="prob")
-batted <- add_preds_from_probs("rf.shift.2", batted, probs.rf.shift.2, lw)
-
 
 # fit kNN model -----------------------------------------------------------
 
-print("Fitting kNN...")
+# print("Fitting kNN...")
 library(caret)
 
 # tune k with 'knn cross validation.R'
@@ -104,30 +101,21 @@ library(caret)
 # currently fitting model on half the data
 # could fit on all data, but would need to re-tune k and it won't make much difference
 # knnmod <- fit_knn_model(batted, k=50, trainSize=0.5, seed=1)
-load("./data/knnmod.RData")
-probs.knn <- predict(knnmod,newdata=batted,type="prob")
-probs.knn <- as.matrix(probs.knn)  # it was returning a list, which caused problems with the next function
-batted <- add_preds_from_probs("knn", batted, probs.knn, lw)
+# load("./data/knnmod.RData")
+# probs.knn <- predict(knnmod,newdata=batted,type="prob")
+# probs.knn <- as.matrix(probs.knn)  # it was returning a list, which caused problems with the next function
+# batted <- add_preds_from_probs("knn", batted, probs.knn, lw)
 
 
 # fit other models --------------------------------------------------------
 
-# Ideas:
-# use methods in caret package: http://topepo.github.io/caret/train-models-by-tag.html#Tree_Based_Model 
-# see this website for info and examples for a lot of these: https://www.analyticsvidhya.com/blog/2017/09/common-machine-learning-algorithms/
-# - Naive Bayes
-# - SVM?
-# - XGBoost (boosted trees. use method="xgbTree" or "xgbLinear")
-# - somehow account for the fact that the RF model underestimates wOBA for faster players
-# - add age into the model, or adjust for future year's prediction
-# - mixed effect vesion of multinomial model
-
-
+# potentially try more models in the future
 print("Done fitting models.")
 
 
 # confusion matrix --------------------------------------------------------
 
+print("Building confusion matrix...")
 preds.multinom <- predict(mod.multinom, newdata=batted)
 preds.rf <- predict(rf.speed, newdata=batted)
 
@@ -164,9 +152,14 @@ sub.lag <- subset(batting_lagged,AB>=AB_cutoff & AB.prev>=AB_cutoff)
 
 # get Marcel projections --------------------------------------------------
 
+print("Creating Marcel projections...")
+
+# 2018 projections
+eval.df.2018 <- get_marcel_eval_df(2018, lw_years=2015:2017, pred_df=batting.dt, include_true_stats=FALSE)
+
+# 2017 projections (for evaluation)
 steamer.2017 <- read.csv("./projections/Steamer projections 2017.csv")
 steamer.2017 <- add_bbref_and_lahman_ids(steamer.2017)
-
 eval.df.2017 <- get_marcel_eval_df(2017, lw_years=2015:2017, pred_df=batting.dt, AB_cutoff=AB_cutoff)
 eval.df.2017 <- add_steamer_to_eval_df(eval.df.2017, steamer.2017)
 
@@ -179,6 +172,7 @@ marcel_eval_plot(eval.df.2017, model_prefix="knn", model_desc="kNN")
 marcel_eval_plot(eval.df.2017, model_prefix="multinom", model_desc="multinom")
 marcel_eval_plot(eval.df.2017, model_prefix="steamer", model_desc="Steamer")
 
+# 2016 projections
 eval.df.2016 <- get_marcel_eval_df(2016, lw_years=2015:2017, pred_df=batting.dt, AB_cutoff=AB_cutoff)
 marcel_eval_plot(eval.df.2016, model_desc="Marcel")
 marcel_eval_plot(eval.df.2016, model_prefix="rf.speed", model_desc="RF")
@@ -186,6 +180,7 @@ marcel_eval_plot(eval.df.2016, model_prefix="rf.shift", model_desc="RF (shift)")
 marcel_eval_plot(eval.df.2016, model_prefix="rf", model_desc="RF (w/o speed)")
 marcel_eval_plot(eval.df.2016, model_prefix="knn", model_desc="kNN")
 
+# 2007 projections
 # match the analysis here:
 #   https://web.archive.org/web/20080111231423/http://www.baseballprospectus.com/unfiltered/?p=564
 #   (Nate Silver got 0.591 correlation with OPS for Marcel projections)
@@ -206,36 +201,6 @@ summary.2017.OPS <- create_eval_summary(eval.df.2017, stat="OPS")
 # kable(summary.2017.wOBA, digits=3)
 # kable(summary.2017.wOBA, digits=3, format="latex")
 
-# future ideas ------------------------------------------------------------
-# - more models
-# - add wind speed and direction from retrosheet data (it's in event files, maybe in game files)
-# - player age/experience
-# - compare to Marcel projection system
-#   - this page describes the methodology: https://www.beyondtheboxscore.com/2016/2/22/11079186/projections-marcel-pecota-zips-steamer-explained-guide-math-is-fun 
-# - how to account for the fact that HRs seem more likely to happen in 2017?
-# - neural network
-#   - exit velocity, launch angle, spray angle
-#   - L/R
-#   - home team
-#   - speed score
-#   - game year and month
-# - consdier using mixed effects model with some additional variables as random effects
-#   - pitcher, opposing team, temperature, inning, etc.
-#   - good example of a baseball article using mixed effect models:
-#     https://www.baseballprospectus.com/news/article/25514/moving-beyond-wowy-a-mixed-approach-to-measuring-catcher-framing/
-# - any way to get confidence intervals for correlation coefficients?
-# - how to evaluate full projections?
-#   - maybe some ideas from here:
-#     https://www.beyondtheboxscore.com/2017/1/8/14189138/pecota-zips-steamer-marcel-projection-systems-graded
-#   - or here:
-#     https://web.archive.org/web/20080111231423/http://www.baseballprospectus.com/unfiltered/?p=564
-# - should I account for shifts? Statcast data has if_fielding_alignment
-  
-
-
-
-
-# to do -------------------------------------------------------------------
 
 
 
