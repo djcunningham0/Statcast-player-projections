b132b72d2303db170bc5e66e153be75d119af68a
djcunningham0 <djcunningham0@gmail.com>
2018-07-26 20:35:17 -0500

    rename data_update_utilities.R and move a few functions into it

--- data_update_utilities.R
+++ data_update_utilities.R
@@ -43,3 +43,45 @@ format_directory_path <- function(path) {
   }
   return(path)
 }
+
+
+#' record today's date in last_updates.csv
+register_updates <- function(which, path) {
+  require(readr, quietly=TRUE, warn.conflicts=FALSE)
+  
+  path <- format_directory_path(path)
+  
+  update_df <- read_csv(paste0(path, "last_updates.csv"), col_types=cols())
+  update_df[,which] <- Sys.Date()
+  write_csv(update_df, paste0(path, "last_updates.csv"))
+}
+
+
+#' Reset last_updates.csv so that every category says it was last updated 1/1/1900
+#'
+#' @param which if specified, only reset this file's date; otherwise reset everything
+reset_last_updates_file <- function(which=NULL,
+                                    path="/Users/Daniel/Documents/University of Chicago/thesis/Statcast linear weights/data/") {
+  require(readr, quietly=TRUE, warn.conflicts=FALSE)
+  require(dplyr, quietly=TRUE, warn.conflicts=FALSE)
+  
+  path <- format_directory_path(path)
+  
+  d <- as.Date("1900-01-01")
+  if (!is.null(which)) {
+    update_df <- read_csv(paste0(path, "last_updates.csv"), col_types=cols())
+    update_df[,which] <- d
+  } else {
+    update_df <- tibble(linear_weights=d,
+                        crosswalk=d,
+                        speed_scores=d,
+                        current_season_summary=d,
+                        completed_seasons_summary=d,
+                        current_season_statcast=d,
+                        completed_seasons_statcast=d,
+                        current_season_full_rebuild=d
+    )
+  }
+  
+  write_csv(update_df, paste0(path, "last_updates.csv"))
+}
--- update_data_files.R
+++ update_data_files.R
@@ -15,7 +15,8 @@
 #' functions can also be run on an ad hoc basis. The reset_last_updates function
 #' can be run to force all of the files to be fully rebuilt on the next run.
 
-source("./data_update_functions.R")
+setwd("/Users/Daniel/Documents/University of Chicago/thesis/Statcast linear weights")
+source("./data_update_utilities.R")
 
 #' wrapper function that calls the other functions defined below
 update_data <- function(season.year=get_current_season_year(),
@@ -332,46 +333,6 @@ format_scraped_fg_data <- function(df, which="full") {
     mutate_at(num.cols, as.numeric)
 
   return(df)
-}
-
-#' record today's date in last_updates.csv
-register_updates <- function(which, path) {
-  require(readr, quietly=TRUE, warn.conflicts=FALSE)
-
-  path <- format_directory_path(path)
-
-  update_df <- read_csv(paste0(path, "last_updates.csv"), col_types=cols())
-  update_df[,which] <- Sys.Date()
-  write_csv(update_df, paste0(path, "last_updates.csv"))
-}
-
-#' Reset last_updates.csv so that every category says it was last updated 1/1/1900
-#'
-#' @param which if specified, only reset this file's date; otherwise reset everything
-reset_last_updates_file <- function(which=NULL,
-                                    path="/Users/Daniel/Documents/University of Chicago/thesis/Statcast linear weights/data/") {
-  require(readr, quietly=TRUE, warn.conflicts=FALSE)
-  require(dplyr, quietly=TRUE, warn.conflicts=FALSE)
-
-  path <- format_directory_path(path)
-
-  d <- as.Date("1900-01-01")
-  if (!is.null(which)) {
-    update_df <- read_csv(paste0(path, "last_updates.csv"), col_types=cols())
-    update_df[,which] <- d
-  } else {
-    update_df <- tibble(linear_weights=d,
-                        crosswalk=d,
-                        speed_scores=d,
-                        current_season_summary=d,
-                        completed_seasons_summary=d,
-                        current_season_statcast=d,
-                        completed_seasons_statcast=d,
-                        current_season_full_rebuild=d
-    )
-  }
-
-  write_csv(update_df, paste0(path, "last_updates.csv"))
 }
 
 
